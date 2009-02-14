5212dc963448602734d4456c16f6cd47f3e4d242
Fabricio Ferracioli <fabricioferracioli@gmail.com>
2009-02-14 19:09:14 -0200

    tentando utilizar da maneira que eh mostrada no exemplo da pagina de documentacao

--- js/delicious.js
+++ js/delicious.js
@@ -1,45 +1,65 @@
 var deliciousUsername = 'fabricioferracioli';
 var bookmarksShowed = 20;
+
+function makeJSONRequest(url, response, params)
+{
+    var params = {};
+    params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+    gadgets.io.makeRequest(url, response, params);
+};
+
+// function response(obj) {
+//     var jsondata = obj.data;
+//     var html = "<strong>Values: </strong><br /><br />";
+//     // Returned JS object can be processed as an associative array
+//     for (var key in jsondata) {
+//         var item = jsondata[key];
+//         html += key + ": ";
+//         html += item + "<br />";
+//     }
+//     document.getElementById('content_div').innerHTML = html;
+// };
+// makeJSONRequest();
+// gadgets.json.parse(str);
+
 document.observe('dom:loaded', function(){
     /* user login at delicious */
     var url = 'http://feeds.delicious.com/v2/json/';
-    var callback = '?count='+bookmarksShowed+'&callback=processDeliciousUserBookmarks';
+    var params = {};
+    params['count'] = bookmarksShowed;
+    var callback = processDeliciousUserBookmarks;
+    /* requesting the json from user bookmarks */
+    makeJSONRequest(url+deliciousUsername, callback, params);
 
-    /* requesting the json from user bookmarks and addind them in the html head section */
-    scr = new Element('script');
-    scr.type = 'text/javascript';
-    scr.src = url+deliciousUsername+callback;
 
-    $$('head')[0].insert(scr);
-
-    /* requesting user information from delicious */
     url = 'http://feeds.delicious.com/v2/json/userinfo/';
-    callback = '?callback=processDeliciousUserInfo';
-
-    scr = new Element('script');
-    scr.type = 'text/javascript';
-    scr.src = url+deliciousUsername+callback;
-
-    $$('head')[0].insert(scr);
+    callback = processDeliciousUserInfo;
+    params = {};
+    /* requesting user information from delicious */
+    makeJSONRequest(url+deliciousUsername, callback, params);
 });
 
 function processDeliciousUserBookmarks(bookmarks){
-    bookmarks.each(function(elem){
-        console.log(elem);
-        a = new Element('a', {href: elem.u}).update(elem.d);
+    var jsondata = elem.data;
+    bookmarks.each(function(jsondata){
+        a = new Element('a', {href: jsondata[u]}).update(jsondata[d]);
         $('delicious').insert(a);
-        $('delicious').insert(' -- saved '+elem.dt);
+        $('delicious').insert(' -- saved '+jsondata[dt]);
         $('delicious').insert(new Element('br'));
-        $('delicious').insert(new Element('q').update(elem.n));
+        $('delicious').insert(new Element('q').update(jsondata[n]));
         $('delicious').insert(new Element('br'));
     });
 }
 
 function processDeliciousUserInfo(info){
+    var jsondata = info.data;
     var person = opensocial.Person.getField(Person.Field.NAME);
-    $('user_data').insert(person+' have '+info[0].n+' bookmaked items');
+    $('user_data').insert(person+' have '+jsondata[0][n]+' bookmaked items');
     $('user_data').insert(new Element('br'));
     $('user_data').insert('showing last '+bookmarksShowed);
     $('user_data').insert(new Element('br'));
     $('user_data').insert(new Element('a', {href: 'Visit '+person+' at delicious'}));
 }
+
+
+
