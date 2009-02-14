ba19e058165b46360d41625665f4ffbe144033a5
Fabricio Ferracioli <fabricioferracioli@gmail.com>
2009-02-14 20:14:46 -0200

    alterando para acessar objetos

--- js/delicious.js
+++ js/delicious.js
@@ -30,11 +30,11 @@ function processDeliciousUserBookmarks(bookmarks){
     var jsondata = bookmarks.data;
     jsondata.each(function(jsdata){
         console.log(jsdata);
-        a = new Element('a', {href: jsdata[u]}).update(jsdata[d]);
+        a = new Element('a', {href: jsdata.u}).update(jsdata.d);
         $('delicious').insert(a);
-        $('delicious').insert(' -- saved '+jsdata[dt]);
+        $('delicious').insert(' -- saved '+jsdata.dt);
         $('delicious').insert(new Element('br'));
-        $('delicious').insert(new Element('q').update(jsdata[n]));
+        $('delicious').insert(new Element('q').update(jsdata.n));
         $('delicious').insert(new Element('br'));
     });
 }
@@ -45,7 +45,7 @@ function processDeliciousUserInfo(info){
     console.log(jsondata);
 
     var person = deliciousUsername;
-    $('user_data').insert(person+' have '+jsondata[0][n]+' bookmaked items');
+    $('user_data').insert(person+' have '+jsondata[0].n+' bookmaked items');
     $('user_data').insert(new Element('br'));
     $('user_data').insert('showing last '+bookmarksShowed);
     $('user_data').insert(new Element('br'));
