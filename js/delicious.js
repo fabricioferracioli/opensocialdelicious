ddb030817b18a3cd72b7719457ea538a7175d10c
Fabricio Ferracioli <fabricioferracioli@gmail.com>
2009-02-14 20:08:15 -0200

    voltando a versao anterior

--- js/delicious.js
+++ js/delicious.js
@@ -6,6 +6,7 @@ function makeJSONRequest(url, response, params)
     var params = {};
     params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
     gadgets.io.makeRequest(url, response, params);
+    console.log('aqui!!!!');
 };
 
 document.observe('dom:loaded', function(){
@@ -17,17 +18,14 @@ document.observe('dom:loaded', function(){
     /* requesting the json from user bookmarks */
     makeJSONRequest(url+deliciousUsername, callback, params);
 
-
     url = 'http://feeds.delicious.com/v2/json/userinfo/';
     callback = processDeliciousUserInfo;
     params = {};
     /* requesting user information from delicious */
     makeJSONRequest(url+deliciousUsername, callback, params);
-    alert('PDL');
 });
 
 function processDeliciousUserBookmarks(bookmarks){
-    alert('aqui');
     console.log(bookmarks);
     var jsondata = bookmarks.data;
     jsondata.each(function(jsdata){
